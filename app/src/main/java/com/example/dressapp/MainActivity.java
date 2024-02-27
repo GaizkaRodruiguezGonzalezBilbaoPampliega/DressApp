ffc5b0232e20ab4f579eab3ae2b07e8c84d6ee1a
Gaizka <gaizka.rodriguezgo@elorrieta-errekamari.com>
2024-02-27 13:23:56 +0100

    Commit menu

--- app/src/main/java/com/example/dressapp/MainActivity.java
+++ app/src/main/java/com/example/dressapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.dressapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this line hide statusbar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new PostsFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_posts);
+
+        navigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                   case R.id.nav_posts:
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.nav_create:
+                        fragment = new CreateFragment();
+                        break;
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+                return true;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/dressapp/entidades/Usuario.java
@@ -0,0 +1,12 @@
+package com.example.dressapp.entidades;
+
+import java.util.List;
+
+public class Usuario {
+
+    private int id;
+    private String nick, nombre;
+    private List<Usuario> seguidores, seguidos;
+
+
+}
